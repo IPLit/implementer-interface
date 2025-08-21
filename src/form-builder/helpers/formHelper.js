b55f17732d5999f88c9177ec416ddcf98476baf4
shilpa <sparab@iplit.in>
2025-08-21 16:30:08 +0530

    Trial for patient form error

--- src/form-builder/helpers/formHelper.js
+++ src/form-builder/helpers/formHelper.js
@@ -20,25 +20,41 @@ export default class FormHelper {
     return pattern.test(formName);
   }
 
+  static processEventsUnescapedHtml(control) {
+    if (control && control.events && control.type === 'obsControl' && control.events !== undefined) {
+      let eventKeys = Object.keys(control.events);
+      eventKeys.forEach(eventKey => {
+        const script = control.events[eventKey];
+        if (script && script !== undefined && script.length > 0) {
+          const scriptModed = ReactHtmlParser(script, {decodeEntities: true});
+          control.events[eventKey] = (scriptModed && scriptModed !== undefined && scriptModed.length > 0) ? scriptModed.at(0) : script;
+        }
+      });
+    }
+    return control ? control.events : {};
+  }
+
   static getObsControlEvents(control) {
     let obsControlEvents = [];
-    if (control.type === 'obsControl' && control.concept !== undefined) {
+    if (control && control.type === 'obsControl' && control.concept !== undefined) {
       obsControlEvents = obsControlEvents.concat({
-        id: control.id, name: control.concept.name,
-        events: ReactHtmlParser(control.events, {decodeEntities: false}),
+        id: control.id, name: control.concept.name, events: this.processEventsUnescapedHtml(control)
       });
-    } else if (control.controls !== undefined) {
+    } else if (control && control.controls !== undefined) {
       const childControls = control.controls;
-      const obsControls = childControls.filter(ctrl => ctrl.type === 'obsControl');
-      const nonObscontrols = childControls.filter(ctrl => ctrl.type !== 'obsControl');
+      const obsControls = childControls.filter(ctrl => ctrl && ctrl.type === 'obsControl');
+      const nonObscontrols = childControls.filter(ctrl => ctrl && ctrl.type !== 'obsControl');
       nonObscontrols.forEach(ctrl => {
         if (ctrl.controls !== undefined) {
           const childObsControlEvents = this.getObsControlEvents(ctrl);
-          obsControlEvents = obsControlEvents.concat(childObsControlEvents);
+          if (childObsControlEvents && childObsControlEvents.length > 0) {
+            obsControlEvents = obsControlEvents.concat(childObsControlEvents);
+          }
         }
       });
-      obsControlEvents = obsControlEvents.concat(obsControls.map(ctrl =>
-        ({ id: ctrl.id, name: ctrl.concept.name, events: ReactHtmlParser(ctrl.events, {decodeEntities: false}) })));
+      obsControls.forEach(ctrl => {
+        obsControlEvents.push({ id: ctrl.id, name: ctrl.concept.name, events: this.processEventsUnescapedHtml(ctrl) })
+      });
     }
     return obsControlEvents;
   }
